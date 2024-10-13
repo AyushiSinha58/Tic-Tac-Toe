f199af9d4f011bac84fc92b4bec1e8ee94c37e50
AyushiSinha58 <ayushisinha53058@gmail.com>
2024-10-13 18:01:45 +0530

    audios

--- intro.js
+++ intro.js
@@ -1,3 +1,10 @@
 function startGame() {
     window.open('main game/tictactoe.html', '_self');
 }
+
+window.addEventListener('load', () => {
+    const audio = document.getElementById('bg_theme');
+
+    // Attempt to autoplay the audio
+    const playPromise = audio.play();
+});
